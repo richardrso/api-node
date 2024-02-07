cb7e6cdd9db291bba3de9154f26b43043af5e51a
rick <55769589+richardrso@users.noreply.github.com>
2024-02-07 11:04:10 -0300

    Remove some comments

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
-const API_ADDRESS = 'http://localhost:3000';// constante com o endereço base da API
-    
-async function get(endpoint) { //função que recebe um endpoint
-  return fetch(API_ADDRESS+endpoint).then(resp => resp.json());
-  //API_ADDRESS envia um GET para esse endereço se ouver reposta positiva, converte para JSON
+const API_ADDRESS = "http://localhost:3000";
+async function get(endpoint) {
+  return fetch(API_ADDRESS + endpoint).then((resp) => resp.json());
 }
 
-function yell(endpoint) { /* unção yell usa a função get()
-para fazer chamadas na API no endpoint e monstra um retorno no alert */
+function yell(endpoint) {
   loading(true);
 
   get(endpoint)
-    .then(data => {
+    .then((data) => {
       alert(`Message: ${data?.message}.\n\nCheck the console for data`);
       console.log(data);
     })
@@ -18,7 +15,6 @@ para fazer chamadas na API no endpoint e monstra um retorno no alert */
     .finally(() => loading(false));
 }
 
-function loading(is = true) {/* se o parâmentro 'is' for true a classe loading é adicionada
-no body se 'is' for false a classe loading é removida */
-  document.body.classList[is ? 'add' : 'remove']('loading');
-} 
+function loading(is = true) {
+  document.body.classList[is ? "add" : "remove"]("loading");
+}
